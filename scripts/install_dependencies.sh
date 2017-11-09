5e4ff920520377a09b51692e1b8f69f2f3a2eb5d
jigisha29 <jigisha.araniya29@gmail.com>
2017-11-09 17:03:27 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,4 +4,4 @@ yum groupinstall -y "Web Server" "MySQL Database" "PHP Support"
 yum install -y php-mysql
 yum install mariadb -y
 yum install mariadb mariadb-server -y
-rm -rf /var/www/html/README.md
+
